2228dda591c1f0ae55edb55fc966de32a0f3a538
Vishnu Prasad <87913417+vishnuserkl29@users.noreply.github.com>
2021-10-18 12:42:08 +0530

    Update updater.js

--- plugins/updater.js
+++ plugins/updater.js
@@ -1,9 +1,9 @@
-/* Copyright (C) 2020 Yusuf Usta.
+/* Copyright (C) 2020 vishnuserkl29/yusufutsa
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-WhatsAsena - Yusuf Usta
+WhatsMeenu - vishnuserkl29
 */
 
 const simpleGit = require('simple-git');
