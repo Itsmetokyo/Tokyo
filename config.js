fe564584cea2fc8411391e93f52babd3b12dab28
Vishnu Prasad <87913417+vishnuserkl29@users.noreply.github.com>
2021-10-18 11:26:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ module.exports = {
     KICKMEMSG: process.env.KICKME_MESSAGE === undefined ? 'default' : process.env.KICKME_MESSAGE,
     AUTOSTICKER: process.env.AUTO_STICKER === undefined ? 'true' : process.env.AUTO_STICKER,
     PLK: process.env.OWNER_NAME === undefined ? 'default' : process.env.OWNER_NAME,
-    THERI_BLOCK: process.env.OFFENSIVE_BLK === undefined ? 'true' : process.env.OFFENSIVE_BLK,
+    INBO1: process.env.OFFENSIVE_BLK === undefined ? 'true' : process.env.OFFENSIVE_BLK,
     FULLEVA: process.env.FULL_EVA === undefined ? 'false' : process.env.FULL_EVA,
     BOT_PRESENCE: process.env.BOT_PRESENCE === undefined ? 'online' : process.env.BOT_PRESENCE,
     BLOCKCHAT: process.env.BLOCK_CHAT === undefined ? false : process.env.BLOCK_CHAT,
