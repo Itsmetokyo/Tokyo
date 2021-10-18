e6a2efd29197778acbe863fd69566a4af5f97870
Vishnu Prasad <87913417+vishnuserkl29@users.noreply.github.com>
2021-10-18 09:35:54 +0530

    Create theriblock.js

---
+++ plugins/theriblock.js
@@ -0,0 +1,74 @@
+/* COPYRIGHT © HISHAM-SOPHIA
+   EDITTED © vishnuserkl29
+*/
+const Hisham = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const exec = require('child_process').exec;
+const os = require("os");
+const fs = require('fs');
+const Config = require('../config')
+
+// Inbox Block System
+// This Plugins By Hisham Muhammed 
+const THERI_BLOCK = "_~~~••• MEENAKSHI_AI •••~~~_* \n\n\n *You have been blocked.* \n\n *REASON:Using offensive words🖐🏻🖐🏻"
+ if (Config.THERI_BLOCK == 'true') {
+Hisham.addCommand({on: 'text', fromMe: false, delownsewcmd: false, onlyPm: true }, (async (message, match) => {
+        let regexb1ichu = new RegExp('thayoli')
+        let regexb2ichu = new RegExp('myre')
+        let regexb3ichu = new RegExp('kunna')
+        let regexb4ichu = new RegExp('poorimole')
+        let regexb5ichu = new RegExp('myr')
+        let regexb6ichu = new RegExp('patti')
+        let regexb7ichu = new RegExp('endi')
+        let regexb8ichu = new RegExp('fuck')
+// export data -(Hisham-muhammed)
+          if (regexb1ichu.test(message.message)) {
+           
+            await message.client.sendMessage(message.jid, '*' + INBO1 + '*', MessageType.text);
+            await message.client.blockUser(message.jid, "add");
+          } 
+        else if (regexb2ichu.test(message.message)) {
+          
+           await message.client.sendMessage(message.jid, '*' + INBO1 + '*', MessageType.text);
+            await message.client.blockUser(message.jid, "add");
+          }
+         else if (regexb3ichu.test(message.message)) {
+           
+            await message.client.sendMessage(message.jid, '*' + INBO1 + '*', MessageType.text);
+            await message.client.blockUser(message.jid, "add");
+          }
+        else if (regexb4ichu.test(message.message)) {
+           
+            await message.client.sendMessage(message.jid, '*' + INBO1 + '*', MessageType.text);
+            await message.client.blockUser(message.jid, "add");
+          }
+          else if (regexb5ichu.test(message.message)) {
+           
+            await message.client.sendMessage(message.jid, '*' + INBO1 + '*', MessageType.text);
+            await message.client.blockUser(message.jid, "add");
+          }
+          else if (regexb6ichu.test(message.message)) {
+           
+            await message.client.sendMessage(message.jid, '*' + INBO1 + '*', MessageType.text);
+            await message.client.blockUser(message.jid, "add");
+          }
+         else if (regexb6ichu.test(message.message)) {
+           
+            await message.client.sendMessage(message.jid, '*' + INBO1 + '*', MessageType.text);
+            await message.client.blockUser(message.jid, "add");
+          }
+         else if (regexb7ichu.test(message.message)) {
+           
+            await message.client.sendMessage(message.jid, '*' + INBO1 + '*', MessageType.text);
+            await message.client.blockUser(message.jid, "add");
+          }
+         else if (regexb8ichu.test(message.message)) {
+           
+            await message.client.sendMessage(message.jid, '*' + INBO1 + '*', MessageType.text);
+            await message.client.blockUser(message.jid, "add");
+          }
+          
+}));
+
+
+}
